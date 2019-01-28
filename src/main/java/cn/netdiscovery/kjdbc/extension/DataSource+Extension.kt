07eb85650f7ae165096ea8be1c4c10df3a232ad7
fengzhizi715 <fengzhizi715@126.com>
2019-01-28 20:43:36 +0800

    add DataSource+Extension

---
+++ src/main/java/cn/netdiscovery/kjdbc/extension/DataSource+Extension.kt
@@ -0,0 +1,37 @@
+package cn.netdiscovery.kjdbc.extension
+
+import java.sql.ResultSet
+import javax.sql.DataSource
+
+/**
+ * Created by tony on 2019-01-28.
+ */
+fun DataSource.update(sql: String, vararg params: Any?) =
+        this.connection.use {
+            it.update(sql, *params)
+        }
+
+inline fun <T> DataSource.selectFirst(sql: String, vararg params: Any?, block: (ResultSet) -> T) =
+        this.connection.use {
+            it.selectFirst(sql, *params, block = block)
+        }
+
+fun DataSource.selectFirst(sql: String, vararg params: Any?) =
+        this.connection.use {
+            it.selectFirst(sql, *params)
+        }
+
+inline fun DataSource.selectEach(sql: String, vararg params: Any?, block: (ResultSet) -> Unit) =
+        this.connection.use {
+            it.selectEach(sql, *params, block = block)
+        }
+
+inline fun <T> DataSource.selectAll(sql: String, vararg params: Any?, block: (ResultSet) -> T) =
+        this.connection.use {
+            it.selectAll(sql, *params, block = block)
+        }
+
+fun DataSource.selectAll(sql: String, vararg params: Any?) =
+        this.connection.use {
+            it.selectAll(sql, *params)
+        }
